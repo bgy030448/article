97054c90b318b1aacc1889efad5eb1edf1bebb3f
bgy030448 <bgy030448@gmail.com>
2026-04-24 17:25:16 +0900

    #3.repository 및 service 설계

--- src/service/ArticleService.java
+++ src/service/ArticleService.java
@@ -3,5 +3,6 @@ package service;
 import repository.ArticleRepository;
 
 public class ArticleService {
-    CruInterface repository = new ArticleRepository();
+    CrudInterface repository = new ArticleDAO();   // DB 버전
+    CruInterface repository = new ArticleRepository();   // 메모리 버전
 }
--- src/service/CommentService.java
+++ src/service/CommentService.java
@@ -3,7 +3,8 @@ package service;
 import repository.ArticleRepository;
 
 public class CommentService {
-    CruInterface dao = new ArticleRepository();
+    CrudInterface dao = new ArticleDAO();
+    // CrudInterface dao = new ArticleRepository(); // 메모리 버전
 
     public CruInterface getDao() {
     }
